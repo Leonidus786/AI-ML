3dfdfa460674e1d3b8daa2abed17694ed64530b5
Leonidus786 <abhi.upadh@gmail.com>
2024-04-06 07:55:59 +0530

    data types

---
+++ Python/Day_2/Data_types/main.py
@@ -0,0 +1,45 @@
+# Python Datatypes.
+
+# 1. Numeric types --> Integers -> int(), Float -> float(), Complex -> complex()
+
+#Integers - > Collection of whole numbers along with negative of natural nos. makes a family of integers.
+
+a = 2
+
+print(a)
+print(type(a))
+
+# Float -> Fractions, decimal nos., Rational nos., Real Nos., Irrational Nos..
+
+b = 98.2
+
+c = -45.8
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+
+# Complex --> A Complex Number is a combination of a Real Number and an Imaginary Number.
+
+d = 2 + 5j
+
+print(d)
+print(type(d))
+
+
+# Text Type --> String -> str()
+
+e = 'Amit'
+
+print(e)
+print(type(e))
+
+f = "Abhishek"
+
+print(f)
+print(type(f))
+
+
---
+++ Python/Day_2/Data_types/main_1.py
@@ -0,0 +1,22 @@
+# Sequential data types -- List, Tuples, Dictionaries
+
+
+#List -- items within a square brackets.
+
+a = [1,2.7,'Amit',4+8j,True]
+
+print(a)
+print(type(a))
+
+#Tuples -- items within parentheses.
+
+b = (1,2.7,'Amit',4+8j,True)
+
+print(b)
+print(type(b))
+
+
+c = {'key':'Value', "Name":"John","Age":30,"Address":"xyzpur"}
+
+print(c)
+print(type(c))
