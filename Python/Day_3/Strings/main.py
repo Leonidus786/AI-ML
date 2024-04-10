81ce2fe861e2422f5f2b19687a8257a54a0d1f6a
Leonidus786 <abhi.upadh@gmail.com>
2024-04-10 07:45:14 +0530

    Strings

---
+++ Python/Day_3/Strings/main.py
@@ -0,0 +1,52 @@
+# "Hello world!"
+
+# print("Hello"[0]) #Position of characters
+
+# Slice operator -- [Start:End], [Start:End:Step]
+
+# Print everything from start till 4th.
+
+# print("Hello"[:4])
+
+# print("Ashmita"[0:3])
+
+# Concatenation -- Combining of two or more than two strings.
+
+# print("Hello" + "Abhishek")  # There should be a space in between two words.
+
+#This would do the same with a space between two words.
+# print("Hello " + "Abhishek") #Space in hello
+# print("Hello" + " Abhishek") # Space before name
+# print("Hello" + " " +"Abhishek") # Space in between two words
+
+# lower()
+
+# print("AbHIShek".lower())
+
+#upper()
+
+# print("amit".upper())
+
+# strip()
+
+# name = "         Abhishek          "
+# print(name.strip())
+
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+# print(myorder.format(quantity, itemno, price))
+
+# print("Hello, World!")
+# print("Hello, World!")
+# print("Hello, World!")
+
+# print("Hello, World!\nHello, World!\nHello, World!")
+
+# msg = "I will be a "Data Scientist" in 2024."
+
+msg = "I will be a \"Data Scientist\" in 2024."
+print(msg)
+msg = 'I will be a "Data Scientist" in 2024.'
+print(msg)
